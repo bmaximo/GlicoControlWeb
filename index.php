092a032a97f2426e784e15b7cce61335c23bd53b
barbara.cmaximo <barbara.cmaximo@hotmail.com>
2016-11-30 05:49:03 -0500

    imagem do slide

--- index.php
+++ index.php
@@ -166,11 +166,8 @@
 						<img alt="Carousel Bootstrap Second" src="img/slide2.png">
 						<div class="carousel-caption">
 							<h4>
-								Second Thumbnail label
+								Dia Mundial do Diabetes
 							</h4>
-							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-							</p>
 						</div>
 					</div>
 				</div> <a class="left carousel-control" href="#carousel-708755" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-708755" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
